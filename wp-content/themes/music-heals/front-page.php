c9ccaf840899f03855cb95b9e33724edcbb52db6
lemuel <reyeslemuel@hotmail.com>
2016-03-22 13:08:15 -0700

    add site content div to frontpage

--- wp-content/themes/music-heals/front-page.php
+++ wp-content/themes/music-heals/front-page.php
@@ -3,10 +3,9 @@
  * The main template file.
  *
  * @package RED_Starter_Theme
- */
+ */ ?>
 
-get_header(); ?>
-
+<div id="content" class="site-content">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -35,6 +34,6 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+</div>
 
 <?php get_footer(); ?>
