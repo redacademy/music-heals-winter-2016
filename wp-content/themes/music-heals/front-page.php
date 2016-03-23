eec3446387b43079d7469199c67b20e64d59fecf
lemuel <reyeslemuel@hotmail.com>
2016-03-23 11:47:16 -0700

    add bxslider and fix content-home.php

--- wp-content/themes/music-heals/front-page.php
+++ wp-content/themes/music-heals/front-page.php
@@ -8,40 +8,6 @@
 	get_header();
  ?>
 
-		<?php get_template_part( 'template-parts/content-home.php' ); ?>
-
-	<ul class="bxslider">
-		<li>
-			<img src="<?php bloginfo( 'template_url' ); ?>/assets/Home_Slider1.svg" alt="music heals images" class="slider-images"/>
-		</li>
-		<li>
-			<img src="<?php bloginfo( 'template_url' ); ?>/assets/Home_Slider2.svg" alt="music heals images" class="slider-images"/>
-		</li>
-		<li>
-			<img src="<?php bloginfo( 'template_url' ); ?>/assets/Home_Slider3.svg" alt="music heals images" class="slider-images"/>
-		</li>
-	</ul>
-
-	<section class="news-and-events-section">
-			<!--  NEWS AND EVENTS -->
-			<div class="news-and-events">
-
-			  <div>
-			    <h1> news and events</h1>
-			  </div>
-
-			  <div>
-			    <h2 class="sub-heading">70 bars across canada
-			        support music therapy
-			        this saturday night
-			    </h2>
-			  </div>
-
-			  <div>
-			    <span>+ More</span>
-			  </div>
-
-			</div>  <!-- ends wrapper div -->
-	</section>
+		<?php get_template_part( 'template-parts/content-home' ); ?>
 
 <?php get_footer(); ?>
---
+++ wp-content/themes/music-heals/template-parts/content-home.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package RED_Starter_Theme
+
+ */
+ ?>
+
+<ul class="bxslider">
+  <li>
+    <img src="<?php bloginfo( 'template_url' ); ?>/assets/Home_Slider1.svg" alt="music heals images" class="slider-images"/>
+  </li>
+  <li>
+    <img src="<?php bloginfo( 'template_url' ); ?>/assets/Home_Slider2.svg" alt="music heals images" class="slider-images"/>
+  </li>
+  <li>
+    <img src="<?php bloginfo( 'template_url' ); ?>/assets/Home_Slider3.svg" alt="music heals images" class="slider-images"/>
+  </li>
+</ul>
+
+<section class="news-and-events-section">
+    <!--  NEWS AND EVENTS -->
+    <div class="news-and-events">
+
+      <div>
+        <h1> news and events</h1>
+      </div>
+
+      <div>
+        <h2 class="sub-heading">70 bars across canada
+            support music therapy
+            this saturday night
+        </h2>
+      </div>
+
+      <div>
+        <span>+ More</span>
+      </div>
+
+    </div>  <!-- ends wrapper div -->
+</section>
