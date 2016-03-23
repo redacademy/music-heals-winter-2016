afe49d7544a6a5d9d0a43b8e5e43d8fd837faafb
Tomas Gustafson <Tomas Gustafson>
2016-03-22 19:41:39 -0700

    Styled boxes for mobile version of site, added images to header and new files to assets folder.

--- wp-content/themes/music-heals/template-parts/content-home.php
+++ wp-content/themes/music-heals/template-parts/content-home.php
@@ -6,3 +6,14 @@
  */
 
 ?>
+
+<div class="video-box">
+  <div class="text-wrapper">
+    <div class="header-text"><h1>Think about all the ways music helps you.</h1></div>
+    <div class="tagline-text"><p>Music Heals wants to make sure that <span>everyone</span> has access to the healing power of music.</p></div>
+  </div>
+</div>
+<div class="donations-box">
+  <h1>The Gift of Music</h1>
+  <button>Give</button>
+</div>
