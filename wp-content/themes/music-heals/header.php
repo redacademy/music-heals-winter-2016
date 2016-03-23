867190cb11968257c556e28975abbf0616a3a415
Sara Sim <sarasim@Saras-MacBook-Air.local>
2016-03-23 11:31:52 -0700

    Add social-icons in footer and edit sizing in 'about' and 'shop' divs.

--- wp-content/themes/music-heals/header.php
+++ wp-content/themes/music-heals/header.php
@@ -12,8 +12,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-		<script src="https://use.typekit.net/phd7iff.js"></script>
-		<script>try{Typekit.load({ async: true });}catch(e){}</script>
 
 	<?php wp_head(); ?>
 	</head>
