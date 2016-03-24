8d95bf8b3af7294e2ba3f7b91ff854fe75dfb117
lemuel <reyeslemuel@hotmail.com>
2016-03-24 13:59:54 -0700

    add mixins for about page and content-about markup

--- wp-content/themes/music-heals/template-parts/content-about.php
+++ wp-content/themes/music-heals/template-parts/content-about.php
@@ -17,26 +17,25 @@
 <!--  WHO WE ARE -->
 
 <section>
-  <div class="who-we-are-banner">
-  </div>
-
-  <h1>Who we are</h1>
+  <div class="who-we-are-banner"></div>
 
   <div class="who-we-are-content-box">
+
+    <h1>Who we are</h1>
+
     <!-- <?php echo CFS()->get( 'who_we_are_copy' ); ?> -->
   </div>
-
 </section>
 
 <!--  HOW WE HELP -->
 
 <section>
-  <div class="how-we-help-banner">
-  </div>
-
-  <h1>How we help</h1>
+  <div class="how-we-help-banner"></div>
 
   <div class="how-we-help-content-box">
+
+    <h1>How we help</h1>
+
     <!-- <?php echo CFS()->get( 'how_we_help_copy' ); ?> -->
   </div>
 </section>
@@ -44,12 +43,12 @@
 <!--  PROGRAMS  -->
 
 <section>
-  <div class="programs-banner">
-  </div>
-
-  <h1>Programs</h1>
+  <div class="programs-banner"></div>
 
   <div class="programs-content-box">
+
+    <h1>Programs</h1>
+
     <!-- <?php echo CFS()->get( 'programs_copy' ); ?> -->
   </div>
 </section>
@@ -57,12 +56,12 @@
 <!--  SUCCESS STORIES-->
 
 <section>
-  <div class="success-stories-banner">
-  </div>
-
-  <h1>Success Stories</h1>
+  <div class="success-stories-banner"></div>
 
   <div class="success-stories-content-box">
+
+    <h1>Success Stories</h1>
+
     <!-- <?php echo CFS()->get( 'success_stories_copy' ); ?> -->
   </div>
 </section>
