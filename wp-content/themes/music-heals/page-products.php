aee8e7814f25f03a4133a5dd778c22d50f289889
Tomas Gustafson <Tomas Gustafson>
2016-03-29 14:33:16 -0700

    Styling front page, resetting gulpfile.

--- wp-content/themes/music-heals/page-products.php
+++ wp-content/themes/music-heals/page-products.php
@@ -10,10 +10,9 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
 			<div class="merch-header"><h1>Merch</h1></div>
 
-      <h1>Kids</h1>
+      <h1 class="merch-category">Kids</h1>
 
 			      <?php
 			        $args= array(
@@ -33,19 +32,19 @@ get_header(); ?>
 						<div class="products-list">
 			        <?php foreach($query as $post) : setup_postdata($post); ?>
 			          <?php if (has_post_thumbnail()) : ?>
-										<div class="single-latest-posts">
-						          <div class="post-image"><?php the_post_thumbnail('large'); ?></div>
+										<div class="single-latest-products">
+						          <div class="product-image"><?php the_post_thumbnail('large'); ?></div>
 						          <?php endif; ?>
-											<div class="post-info">
+											<div class="product-info">
 							          <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-							          <span class="product-type"><?php echo CFS()->get('product_type'); ?></span>
+							          <span class="product-type"><?php echo CFS()->get('type'); ?></span>
                         <span class="product-price"><?php echo CFS()->get( 'price' ); ?></span>
 											</div>
 										</div>
 			        <?php endforeach; wp_reset_postdata(); ?>
 						</div>
 
-          <h1>Womens</h1>
+          <h1 class="merch-category">Womens</h1>
             <?php
 			        $args= array(
                 'post_type'=> 'product',
@@ -64,19 +63,19 @@ get_header(); ?>
 						<div class="products-list">
 			        <?php foreach($query as $post) : setup_postdata($post); ?>
 			          <?php if (has_post_thumbnail()) : ?>
-										<div class="single-latest-posts">
-						          <div class="post-image"><?php the_post_thumbnail('large'); ?></div>
+										<div class="single-latest-products">
+						          <div class="product-image"><?php the_post_thumbnail('large'); ?></div>
 						          <?php endif; ?>
-											<div class="post-info">
+											<div class="product-info">
 							          <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-							          <span class="product-type"><?php echo CFS()->get('product_type'); ?></span>
+							          <span class="product-type"><?php echo CFS()->get('type'); ?></span>
                         <span class="product-price"><?php echo CFS()->get( 'price' ); ?></span>
 											</div>
 										</div>
 			        <?php endforeach; wp_reset_postdata(); ?>
 						</div>
 
-          <h1>Mens</h1>
+          <h1 class="merch-category">Mens</h1>
             <?php
 			        $args= array(
                 'post_type'=> 'product',
@@ -95,12 +94,12 @@ get_header(); ?>
 						<div class="products-list">
 			        <?php foreach($query as $post) : setup_postdata($post); ?>
 			          <?php if (has_post_thumbnail()) : ?>
-										<div class="single-latest-posts">
-						          <div class="post-image"><?php the_post_thumbnail('large'); ?></div>
+										<div class="single-latest-products">
+						          <div class="products-image"><?php the_post_thumbnail('large'); ?></div>
 						          <?php endif; ?>
-											<div class="post-info">
+											<div class="product-info">
 							          <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-							          <span class="product-type"><?php echo CFS()->get('product_type'); ?></span>
+							          <span class="product-type"><?php echo CFS()->get('type'); ?></span>
                         <span class="product-price"><?php echo CFS()->get( 'price' ); ?></span>
 											</div>
 										</div>
